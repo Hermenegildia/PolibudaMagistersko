c53b0d41ddb3e6c0338ace4b48cb5fb577db29f1
Hermenegildia <ala_sz@yahoo.co.uk>
2014-06-15 01:55:45 +0200

    additions

---
+++ MVVM/WpfApplication2/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2
+{
+    public class Customer :INotifyPropertyChanged
+    {
+        public Customer()
+        {
+            this.Name = "Janek Nowaczyk";
+        }
+
+        public Customer(string customerName)
+        {
+            this.Name = customerName;
+        }
+
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
---
+++ MVVM/WpfApplication2/CustomerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication2
+{
+    public class CustomerViewModel : DependencyObject
+    {
+        public static readonly DependencyProperty CustomerProperty =
+            DependencyProperty.Register(
+            "Customer",
+            typeof(Customer),
+            typeof(CustomerViewModel),
+            new PropertyMetadata(null));
+
+        public Customer Customer
+        {
+            get { return (Customer)GetValue(CustomerProperty); }
+            set { SetValue(CustomerProperty, value); }
+        }
+
+    }
+}
